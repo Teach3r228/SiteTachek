e7b24528f0bc325e86c5c1409712191fb190a2f5
Teach3r228 <52379697+Teach3r228@users.noreply.github.com>
2019-10-11 00:31:01 +0700

    Add files via upload

---
+++ Shop/Shop/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.interfaces;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _carsCategories;
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iAllCars;
+            _carsCategories = iCarsCat;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с автомобилями";
+            CarsListViewModel obj = new CarsListViewModel();
+            obj.AllCars = _allCars.Cars;
+            obj.currentCategory = "Автомобили";
+            return View(obj);
+        }
+    }
+}
---
+++ Shop/Shop/Data/mocks/mockCars.cs
@@ -0,0 +1,33 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class mockCars : IAllCars
+    {
+        private readonly ICarsCategory _carsCategory = new MockCategory();
+
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car  { name= "Tesla",shortDesc="Быстрый автомобиль",longDesc="Быстрый автомобиль, комфортабельбный",img="/img/tesla.jpg",price=4500,isFavorite=true,available=true,Category =  _carsCategory.AllCategories.First()},
+                    new Car {name= "Жигуль",shortDesc="Тихий и спокойный",longDesc="Удобный автомобиль для городской жизни",img="/img/Zhigul.jpg",price=4500,isFavorite=true,available=true,Category =  _carsCategory.AllCategories.Last() }
+                };
+            }
+        }
+        public IEnumerable<Car> GetFavCars { get; set; }
+
+
+        public Car getObjectCar(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
